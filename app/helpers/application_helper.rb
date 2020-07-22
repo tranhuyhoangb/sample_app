4cccf5668b341cd124f3587a1ebf3694db78e2d2
Tran Huy Hoang <68525190+tranhuyhoangb@users.noreply.github.com>
2020-07-22 14:54:02 +0700

    Revert "Chapter 345"

--- app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-module ApplicationHelper
-	def full_title page_title = ""
-		base_title = "Ruby on Rails Tutorial Sample App"
-		base_title.empty? ? base_title : "#{page_title} | #{base_title}" 
-	end	
-end
