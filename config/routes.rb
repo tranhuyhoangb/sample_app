ce7e8612df4bb2a13b4a61dd4ca0b1ec34328f8f
tran.huy.hoangb@sun-asterisk.com <tran.huy.hoangb@sun-asterisk.com>
2020-07-27 08:47:10 +0700

    Chapter5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   scope"(:locale)",locale:/en|vi/ do
-    get "static_pages/home"
-    get "static_pages/help"
-    get "static_pages/contact"
-    get "static_pages/about"
-  end 
+    root "static_pages#home"
+
+    get "help", to: "static_pages#help"
+    get "home", to: "static_pages#home"
+    get "contact", to: "static_pages#contact"
+    get "about", to: "static_pages#about"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
