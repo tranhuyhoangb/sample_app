0b8594da5098004ed4e3a8ba3200974826160da2
tran.huy.hoangb@sun-asterisk.com <tran.huy.hoangb@sun-asterisk.com>
2020-07-27 08:47:10 +0700

    chapter3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-	def full_title page_title = ""
-		base_title = "Ruby on Rails Tutorial Sample App"
-		base_title.empty? ? base_title : "#{page_title} | #{base_title}" 
-	end	
+  def full_title page_title
+    base_title = t ".base_title"
+    base_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
