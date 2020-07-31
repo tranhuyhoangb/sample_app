bbe2b3dc4981947195c3ced6662ebd003513ac76
tran.huy.hoangb@sun-asterisk.com <tran.huy.hoangb@sun-asterisk.com>
2020-07-31 10:07:04 +0700

    chapter7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t(".flash", username: @user.name)
+      redirect_to user_path(@user)
+    else
+      flash[:success] = "Create  Fail! >.<"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit User::USERS_PARAMS
+  end
+
 end
