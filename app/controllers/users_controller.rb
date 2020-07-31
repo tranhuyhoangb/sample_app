e75dafb44ec8de2b1ce18ae889bf4d3f8bed88e6
tran.huy.hoangb@sun-asterisk.com <tran.huy.hoangb@sun-asterisk.com>
2020-07-31 10:07:04 +0700

    chapter8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t(".flash", username: @user.name)
-      redirect_to user_path(@user)
+      log_in @user
+      flash[:success] = t ".flash", username: @user.name
+      redirect_to user_path @user
     else
-      flash[:success] = "Create  Fail! >.<"
+      flash[:success] = t ".fail"
       render :new
     end
   end
