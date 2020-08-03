e529bfdf234380b5af25df5c02393d540bffb988
tran.huy.hoangb@sun-asterisk.com <tran.huy.hoangb@sun-asterisk.com>
2020-08-04 06:30:03 +0700

    chapter9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
-  def new;  end
+  def new; end
 
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == Settings.form.check_box ? remember(user) : forget(user)
       redirect_to user_path locale, user
     else
       flash.now[:danger] = t ".danger"
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.validations.user.email_regex
   USERS_PARAMS = %i(name email password password_confirmation).freeze
 
-  validates :name, presence: true, length: {maximum: Settings.validations.user.max_name_length}
-  validates :email, presence: true, length: {maximum: Settings.validations.user.max_email_length}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.validations.user.min_password_length}
+  attr_accessor :remember_token
+
+  validates :name, presence: true,
+    length: {maximum: Settings.validations.user.max_name_length}
+  validates :email, presence: true,
+    length: {maximum: Settings.validations.user.max_email_length},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.validations.user.min_password_length}
 
   before_save :downcase_email
 
@@ -21,6 +27,24 @@ class User < ApplicationRecord
       BCrypt::Password.create string, cost: cost
     end
 
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update remember_digest: nil
   end
 
   private
